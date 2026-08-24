36241f06e5339e4d9c9458b34a5810f6eac66c26
Sztankó Dániel <sztankod@gmail.com>
2026-08-24 15:59:45 +0200

    Remove cloud shape beneath logo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "hu.roadrecord.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 103
+        versionCode = 104
         versionName = "1.02"
     }
     val stableKeystore = System.getenv("ROADRECORD_KEYSTORE")
