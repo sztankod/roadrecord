5aeed4d26226b84480ecf540f7bcfb788f1b2456
Sztankó Dániel <sztankod@gmail.com>
2026-08-28 21:24:45 +0200

    Release 1.27 with updated stop controls

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "hu.roadrecord.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 128
-        versionName = "1.26"
+        versionCode = 129
+        versionName = "1.27"
     }
     val stableKeystore = System.getenv("ROADRECORD_KEYSTORE")
     signingConfigs {
